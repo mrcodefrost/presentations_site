f7fdc0bad7adb7d423f8090f4d09bb09296b4509
mrcodefrost <mr.codefrost@gmail.com>
2024-11-28 18:10:32 +0530

    Landing page done

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -3,12 +3,12 @@ import { FaArrowUpLong } from "react-icons/fa6";
 const LandingPage = () => {
     return (
         <div className="w-full h-screen bg-zinc-900 pt-1">
-            <div className="textstructure mt-52 px-20">
+            <div className="textstructure mt-48 px-20">
                 {["We Create", "Eye opening", "Presentations"].map((item, index) => {
                     return <div key={index} className="masker">
-                        <div className="w-fit flex">
-                            {index === 1 && (<div className="w-[5.5vw] h-[4vw] relative  bg-red-50 leading-[1vw]"> </div>)}
-                            <h1 className="uppercase text-[5.5vw] leading-[6vw] tracking-tighter font-['Founders_Grotesk'] font-medium">
+                        <div className="w-fit flex items-end overflow-hidden">
+                            {index === 1 && (<div className="mr-[1vw] rounded-md w-[8vw] h-[4vw] -top-[1.2vw] relative bg-red-50"> </div>)}
+                            <h1 className="pt-[2vw]-mb-[1vw] uppercase text-[5.5vw] leading-[6.5vw] tracking-tight font-['Founders_Grotesk'] font-medium">
                                 {item}
                             </h1>
                         </div>
@@ -16,7 +16,7 @@ const LandingPage = () => {
                 })}
 
             </div>
-            <div className="border-t-[1px] border-zinc-800 mt-32 flex justify-between items-center py-5 px-20">
+            <div className="border-t-[1px] border-zinc-800 mt-20 flex justify-between items-center py-5 px-20">
                 {["For public and private use companies", "From the first pitch to IPO"].map((item, index) => (
                     <p className="text-md font-light tracking-tight leading-none ">
                         {item}
@@ -27,7 +27,6 @@ const LandingPage = () => {
                         Start the project
                     </div>
                     <div className="w-10 h-10 flex items-center justify-center border-[2px] border-zinc-500 rounded-full">
-
                         <span className="rotate-[45deg">
                             <FaArrowUpLong />
                         </span>
